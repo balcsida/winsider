ce8f09d4cb9fbc9613cb7b206da03e511104bf71
Dávid Balatoni <balcsida@gmail.com>
2016-08-15 00:19:13 +0200

    Add Secondary menu

--- functions.php
+++ functions.php
@@ -45,6 +45,7 @@ function winsider_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => esc_html__( 'Primary', 'winsider' ),
+		'secondary' => esc_html__( 'Secondary', 'winsider' ),
 	) );
 
 	/*
