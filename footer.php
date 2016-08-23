35a4228bfd94c34ba3ddde179417b08dbf9d6eb9
Dávid Balatoni <balcsida@gmail.com>
2016-08-23 04:22:47 +0200

    Footer

--- footer.php
+++ footer.php
@@ -10,17 +10,20 @@
  */
 
 ?>
+	<footer class="bg-black" role="contentinfo">
+		<div class="col-12 sm-col-10 mx-auto">
+			<div class="flex justify-between items-center py2">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="text-decoration-none"><h1 class="h2 white ml1 my2 fw-700"><?php bloginfo( 'name' ); ?></h1></a>
+				<div>
+					<nav class="inline-block h5">
+						<?php echo strip_tags(wp_nav_menu( array( 'theme_location' => 'footer', 'container' => '', 'link_before' => '', 'items_wrap' => '%3$s', 'container' => false , 'echo' => false) ), '<a><div>'); ?>
+					</nav>
+					<div class="inline-block white bold h6 pl2"><?php bloginfo( 'name' ); ?> 2016</div>
+				</div>
+			</div>
+		</div>
+	</footer>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'winsider' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'winsider' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'winsider' ), 'winsider', '<a href="https://balcsida.hu" rel="designer">Dávid Balatoni (balcsida)</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
 
 <?php wp_footer(); ?>
 
