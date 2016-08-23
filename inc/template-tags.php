dee57c4b78cd1b1a4bc07e374d79661807d58485
Dávid Balatoni <balcsida@gmail.com>
2016-08-23 04:23:55 +0200

    extra template tag functions

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -9,9 +9,10 @@
 
 if ( ! function_exists( 'winsider_posted_on' ) ) :
 /**
- * Prints HTML with meta information for the current post-date/time and author.
+ * Prints HTML with meta information for the current post-date/time and categories.
  */
 function winsider_posted_on() {
+
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
 		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
@@ -24,17 +25,28 @@ function winsider_posted_on() {
 		esc_html( get_the_modified_date() )
 	);
 
-	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'winsider' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-	);
+	echo '<span class="posted-on">' . $time_string . '</span>'; // WPCS: XSS OK.
+
+	$categories_list = get_the_category_list( esc_html__( ', ', 'winsider' ) );
+	if ( $categories_list && winsider_categorized_blog() ) {
+		echo  '<span class="cat-links">' . $categories_list . '</span>'; // WPCS: XSS OK.
+	}
+
+}
+endif;
+
+if ( ! function_exists( 'winsider_post_author' ) ) :
+/**
+ * Prints HTML with meta information for the current post-date/time and author.
+ */
+function winsider_post_author() {
 
 	$byline = sprintf(
 		esc_html_x( 'by %s', 'post author', 'winsider' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	echo '<span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
 
 }
 endif;
@@ -46,12 +58,6 @@ if ( ! function_exists( 'winsider_entry_footer' ) ) :
 function winsider_entry_footer() {
 	// Hide category and tag text for pages.
 	if ( 'post' === get_post_type() ) {
-		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( esc_html__( ', ', 'winsider' ) );
-		if ( $categories_list && winsider_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'winsider' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-		}
-
 		/* translators: used between list items, there is a space after the comma */
 		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'winsider' ) );
 		if ( $tags_list ) {
